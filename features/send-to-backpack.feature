d716722a789daecd15eae8c0041922e7523ba150
Mike Larsson <mikelarssonftw@gmail.com>
2013-08-20 17:12:45 -0400

    Implemented three basic scenarios

--- features/send-to-backpack.feature
+++ features/send-to-backpack.feature
@@ -4,8 +4,17 @@ Feature: Send to Backpack
   sites, I want those sites to offer me a Send to Backpack option.
 
   Scenario: Earned a badge and has a Backpack
-    Given I have earned a badge as some@email.org
-    When I click the **Add to Backpack** button
-    And I log in to my Backpack as some@email.org
-    And I accept all conditions/prompts presented to me
+    Given I have earned a badge
+    When I send it to my Backpack
     Then I should see my badge in my Backpack
+
+  Scenario: Reject sending an earned badge
+    When a pushy Issuer starts sending a badge to my Backpack
+    But I reject it
+    Then I should not see it in my Backpack
+
+  Scenario: Sending a badge already in Backpack
+    Given I have a badge in my Backpack
+    When I start to send that badge to my Backpack again
+    Then I should see a notice that I already have that badge
+
--- features/step_definitions/send-to-backpack-steps.js
+++ features/step_definitions/send-to-backpack-steps.js
@@ -1,21 +1,71 @@
+const should = require('should');
+
+function Badge() {
+  var self = this;
+  return self;
+}
+
+function IssuerSite() {
+  var self = this;
+  
+  self.issueBadge = function() {
+    return new Badge();
+  };
+
+  self.sendBadgeTo = function(badge, backpack) {
+    if (backpack.indexOf(badge) !== -1)
+      return { duplicate: true };
+    return {
+      accept: function() {
+        backpack.push(badge);
+      },
+      reject: function() {}
+    };
+  };
+
+  return self;
+}
+
 module.exports = require('../../test/lib/fiber-cucumber')(function() {
-  this.Given(/^I have earned a badge as (.+)$/, function(email) {
-    this.pending();
+  this.Before(function(){
+    this.site = new IssuerSite();
+    this.backpack = [];
   });
 
-  this.When(/^I click the \*\*Add to Backpack\*\* button$/, function() {
-    this.pending();
+  this.Given(/^I have earned a badge$/, function() {
+    this.badge = this.site.issueBadge();
   });
 
-  this.When(/^I log in to my Backpack as (.+)$/, function(email) {
-    this.pending();
+  this.Given(/^I have a badge in my Backpack$/, function() {
+    this.badge = this.site.issueBadge();
+    this.site.sendBadgeTo(this.badge, this.backpack).accept();
   });
 
-  this.When(/^I accept all conditions\/prompts presented to me$/, function() {
-    this.pending();
+  this.When(/^I (start to )?send .* to my Backpack.*$/, function(startOnly) {
+    this.sendToBackpackRequest = this.site.sendBadgeTo(this.badge, this.backpack);
+    if (!startOnly)
+      this.sendToBackpackRequest.accept();
+  });
+
+  this.When(/^a pushy Issuer starts sending a badge to my Backpack$/, function() {
+    this.badge = this.site.issueBadge();
+    this.sendToBackpackRequest = this.site.sendBadgeTo(this.badge, this.backpack);
+  });
+
+  this.When(/^I reject it$/, function() {
+    this.sendToBackpackRequest.reject();
   });
 
   this.Then(/^I should see my badge in my Backpack$/, function() {
-    this.pending();
+    this.backpack.should.include(this.badge);
   });
+
+  this.Then(/^I should not see it in my Backpack$/, function() {
+    this.backpack.should.not.include(this.badge);
+  });
+
+  this.Then(/^I should see a notice that I already have that badge$/, function() {
+    this.sendToBackpackRequest.duplicate.should.be.true;
+  });
+
 });
