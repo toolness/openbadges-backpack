6c95f692569455f1a87933ae8e0cffda2881c092
Mike Larsson <mikelarssonftw@gmail.com>
2013-08-28 16:58:01 -0400

    Made acceptance tests use real SendToBackpackRequest and SendToBackpackRequest.Group objects. This involved upgrading the acceptance test notion of badges to urls backed by actual badgehost assertions, as well as enhancing the internal FakeBackpack class.
    Also added some more detailed assertions to a few step definitions.

--- features/support/env.js
+++ features/support/env.js
@@ -1,92 +1,97 @@
 var should = require('should');
-
+var badgehost = require('badgehost');
+var validator = require('openbadges-validator');
 var fiberize = require('../../test/lib/fiber-cucumber');
+var Future = require('fibers/future');
+var SendToBackpackRequest = require('../../').SendToBackpackRequest;
+var SendToBackpackRequestGroup = SendToBackpackRequest.Group;
+
+var badgehostApp;
 
 var DEFAULT_EMAIL = "me@example.org";
 
-function Backpack() {
+var FakeBackpack = function(owner) {
   var self = [];
 
-  self.owner = DEFAULT_EMAIL;
+  self.owner = owner;
 
-  return self;
-}
-
-function Badge(options) {
-  var self = this;
-
-  self.recipient = options.recipient;
-
-  return self;
-}
-
-function SendToBackpackRequest(backpack, badge) {
-  var self = {result: null};
-
-  self.accept = function() {
-    should.equal(self.result, null);
-    backpack.push(badge);
-    self.result = "accepted";
+  self.has = function(guid, cb) {
+    return cb(null, self.indexOf(guid) != -1)
   };
 
-  self.reject = function() {
-    should.equal(self.result, null);
-    self.result = "rejected";
+  self.indexOfBadgeWithUrl = function(url) {
+    var getAssertionGUID = Future.wrap(validator.getAssertionGUID);
+    var guid = getAssertionGUID(url).wait();
+    return Array.prototype.indexOf.call(self, guid);
   };
 
-  if (backpack.owner != badge.recipient)
-    self.result = "recipient_mismatch";
-
-  if (backpack.indexOf(badge) !== -1)
-    self.result = "exists";
-
-  return self;
-}
-
-function SendToBackpackRequestGroup(badges, backpack) {
-  var self = new Array();
-
-  badges.forEach(function(badge) {
-    self.push(SendToBackpackRequest(backpack, badge));
-  });
-
-  self.acceptAll = function() {
-    self.forEach(function(request) { request.accept(); });
-  };
-
-  self.rejectAll = function() {
-     self.forEach(function(request) { request.reject(); });
+  self.receive = function(info, cb) {
+    info.guid.should.be.a('string');
+    self.indexOf(info.guid).should.eql(-1);
+    self.push(info.guid);
+    cb(null);
   };
 
   return self;
-}
+};
+
+var badgeFor = function(recipient, uid) {
+  return badgehostApp.url('demo.json', {
+    set: {
+      uid: uid || 'uid-1',
+      recipient: {
+        type: "email",
+        hashed: false,
+        identity: recipient
+      }
+    }
+  })
+};
 
 function IssuerSite() {
   var self = this;
-  
+ 
+  var uid_n = 1;
+
   self.loggedInUser = DEFAULT_EMAIL;
-  self.issuedBadges = [];
+  self.issuedBadges = []; // NOTE: issued, not sent
 
   self.issueBadge = function() {
-    self.issuedBadges.push(new Badge({recipient: self.loggedInUser}));
+    self.issuedBadges.push(badgeFor(self.loggedInUser, 'uid-' + uid_n++));
   };
 
   self.sendBadgesTo = function(backpack) {
-    return SendToBackpackRequestGroup(self.issuedBadges, backpack);
+    // Thimble-style always send all
+    return SendToBackpackRequestGroup(backpack, self.issuedBadges);
   };
 
   return self;
 }
 
-should.Assertion.prototype.includeEach = function(array) {
+should.Assertion.prototype.includeBadges = function(array) {
+  array.length.should.be.above(0);
   array.forEach(function(item) {
-    this.include(item);
+    this.assert(
+      ~this.obj.indexOfBadgeWithUrl(item), 
+      function(){ return 'expected backpack to include badge with url ' + item },
+      function(){ return 'expected backpack to not include badge with url ' + item }
+    );
   }, this);
 };
 
 module.exports = fiberize(function() {
   this.Before(function(){
     this.site = new IssuerSite();
-    this.backpack = new Backpack();
+    this.backpack = new FakeBackpack(DEFAULT_EMAIL);
+    badgehostApp = badgehost.app.build();
+    var doneListening = new Future();
+    badgehostApp.listen(function() {
+      doneListening.return(this);
+    });
+    badgehostApp.server = doneListening.wait();
+  });
+
+  this.After(function(){
+    badgehostApp.server.close();
   });
 });
