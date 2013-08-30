baf409228ee3cfdfa643dcd9ea05974b5fcc167b
Atul Varma <varmaa@gmail.com>
2013-08-29 21:08:15 -0400

    Added a page at /test/issue allowing test badges to be viewed.
    Right now they're just links to badgehost-hosted JSON blobs. Once
    we implement the issuer API, they will actually be links to push
    the badges to your backpack.

--- test/lib/index.js
+++ test/lib/index.js
@@ -1,9 +1,32 @@
 var path = require('path');
 var express = require('express');
+var nunjucks = require('nunjucks');
+var async = require('async');
+
+var testUtil = require('./util');
 
 var TEST_STATIC_DIR = path.join(__dirname, '..', 'static');
+var TEST_TEMPLATE_DIR = path.join(__dirname, '..', 'template');
+
+function badgehostAppLazyLoader(app) {
+  var badgehostSetupQueue = async.queue(function(task, cb) {
+    if (app.badgehostApp) return cb(null);
+    require('./util').BadgehostApp(function(err, badgehostApp) {
+      if (err) return cb(err);
+      app.badgehostApp = badgehostApp;
+      cb(null);
+    });
+  }, 1);
+
+  return function(req, res, next) {
+    badgehostSetupQueue.push({}, next);
+  }
+}
 
 exports.defineMiddleware = function(app) {
+  var loader = new nunjucks.FileSystemLoader(TEST_TEMPLATE_DIR);
+
+  app.nunjucksEnv.loaders.push(loader);
   app.use('/test', function(req, res, next) {
     var policies = res.contentSecurityPolicies;
 
@@ -16,4 +39,18 @@ exports.defineMiddleware = function(app) {
     next();
   });
   app.use('/test', express.static(TEST_STATIC_DIR));
+  app.use('/test/issue', badgehostAppLazyLoader(app));
+};
+
+exports.defineRoutes = function(app) {
+  app.get('/test/issue', function(req, res, next) {
+    return res.render('test-issue.html', {
+      badgeFor: app.badgehostApp.badgeFor.bind(app.badgehostApp)
+    });
+  });
+  app.post('/test/issue', function(req, res, next) {
+    var email = req.param('email');
+
+    return res.redirect(app.badgehostApp.badgeFor(email));
+  });
 };
