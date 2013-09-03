95bba989aafd388d0e2ce6861112b434de9c1d37
Atul Varma <varmaa@gmail.com>
2013-09-03 09:51:32 -0400

    added tests for GET /issue

--- test/issue.test.js
+++ test/issue.test.js
@@ -1,3 +1,4 @@
+var querystring = require('querystring');
 var issue = require('../');
 
 var testUtil = require('./lib/util');
@@ -15,8 +16,26 @@ after(function() {
 
 describe("GET /issue", function() {
   it("should return 200 when no query args are given", function(done) {
+    // TODO: We should probably test to ensure that some kind
+    // of error message is displayed here.
     request()
       .get('/issue')
       .expect(200, done);
+  });
+
+  it("should show errors when badges are invalid", function(done) {
+    request()
+      .get('/issue?assertion=u')
+      .expect(/not a valid signed badge or url/)
+      .expect(200, done);
+  });
+
+  it("should present info on valid badges", function(done) {
+    request()
+      .get('/issue?' + querystring.stringify({
+        assertion: badgehostApp.badgeFor('foo@example.org')
+      }))
+      .expect(/Please log in/)
+      .expect(/Demo Badge/, done);
   });
 });
